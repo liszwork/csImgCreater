235724a447fb07aa606af76592710684695ae403
y <zz.iz.0pnup@gmail.com>
2019-04-10 22:52:16 +0900

    ホワイトノイズの更新を追加

--- Noisy/Form1.Designer.cs
+++ Noisy/Form1.Designer.cs
@@ -28,7 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pic = new System.Windows.Forms.PictureBox();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.textLog = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
             this.SuspendLayout();
             // 
@@ -36,26 +40,56 @@
             // 
             this.pic.Location = new System.Drawing.Point(0, 0);
             this.pic.Name = "pic";
-            this.pic.Size = new System.Drawing.Size(802, 450);
+            this.pic.Size = new System.Drawing.Size(427, 328);
             this.pic.TabIndex = 0;
             this.pic.TabStop = false;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(447, 12);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(144, 48);
+            this.buttonUpdate.TabIndex = 1;
+            this.buttonUpdate.Text = "更新";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // textLog
+            // 
+            this.textLog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textLog.Location = new System.Drawing.Point(447, 198);
+            this.textLog.Multiline = true;
+            this.textLog.Name = "textLog";
+            this.textLog.Size = new System.Drawing.Size(366, 129);
+            this.textLog.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(825, 339);
+            this.Controls.Add(this.textLog);
+            this.Controls.Add(this.buttonUpdate);
             this.Controls.Add(this.pic);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.TextBox textLog;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Noisy/Form1.cs
+++ Noisy/Form1.cs
@@ -14,31 +14,72 @@ namespace Noisy
     {
         public readonly int SIZE = 256;
         public readonly int SEED = 1000;
+        Random rand = new Random(1000);
+        private int count = 0;
+
         public Form1()
         {
             InitializeComponent();
             this.pic.Image = CreateWhiteNoise();
+            timer1.Stop();
         }
         public Bitmap CreateWhiteNoise()
         {
             double[,] noise = new double[SIZE, SIZE];
-            Random r = new Random(1000);
             Bitmap bmp = new Bitmap(SIZE, SIZE);
             for ( int y = 0; y < SIZE; y++ )
             {
-                string msg = "";
+                string msg = "@";
                 for ( int x = 0; x < SIZE; x++ )
                 {
-                    float value = (float)r.NextDouble();
+                    float value = GetFRand();
                     msg += value.ToString() + " ";
 
                     int v = (int)(value * 100.0);
-                    Color color = Color.FromArgb(255, v, v, v);
+                    Color color = Color.FromArgb(255, v, v, v + 100);
                     bmp.SetPixel(x, y, color);
                 }
-                Console.Write(msg + "\n");
+                //UpdateTextLog(msg);
             }
             return bmp;
+        }
+
+        private float GetFRand()
+        {
+            return (float)this.rand.NextDouble();
+        }
+
+        private void UpdateTextLog(string log, bool isClear = false)
+        {
+            if ( isClear )
+            {
+                this.textLog.Text = "";
+            }
+            this.textLog.Text = log + "\r\n" + this.textLog.Text;
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            string frand = GetFRand().ToString();
+            UpdateTextLog(string.Format("{0:f5}", frand));
+
+            this.pic.Image = CreateWhiteNoise();
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if ( count < 1000 )
+            {
+                this.pic.Image = CreateWhiteNoise();
+                count++;
+            }
+            else
+            {
+                timer1.Stop();
+                count = 0;
+            }
+            UpdateTextLog(count.ToString(), true);
         }
     }
 }
